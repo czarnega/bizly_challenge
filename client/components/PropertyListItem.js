ca5464217a869c446423d6eff66b332a5d267b86
Colin Zarnegar <c.zarnegar@gmail.com>
2016-08-05 23:33:34 -0700

    converts propertyListItem component to class component to manage state

--- client/components/PropertyListItem.js
+++ client/components/PropertyListItem.js
@@ -1,36 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropertyTag from './PropertyTag';
 
-const PropertyListItem = ({ id, attributes, tags }) => {
-	return (
-		<div className='property-list-item card' id={`card-${id}`}>
-			<img className='card-img' src={attributes.image_url} />
-			<div className='card-content'>
-				<div className='card-header'>
-					<div>
-						<h3 className='property-name'>{attributes.name}</h3>
-						<div className='card-address'>{attributes.display_address}</div>
+class PropertyListItem extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			open: false
+		}
+		this.toggleOpen = this.toggleOpen.bind(this);
+	}
+	toggleOpen(){
+		this.setState({
+			open: !this.state.open
+		})
+	}
+	render(){
+		return (
+			<div className='property-list-item card' id={`card-${this.props.id}`} onClick={this.toggleOpen}>
+				<img className='card-img' src={this.props.attributes.image_url} />
+				<div className='card-content'>
+					<div className='card-header'>
+						<div>
+							<h3 className='property-name'>{this.props.attributes.name}</h3>
+							<div className='card-address'>{this.props.attributes.display_address}</div>
+						</div>
+						<div>
+							<span className='starting-at'>Starting At</span>
+							<h3 className='price'>${this.props.attributes.starting_at}</h3>
+						</div>
 					</div>
-					<div>
-						<span className='starting-at'>Starting At</span>
-						<h3 className='price'>${attributes.starting_at}</h3>
+					<div className='card-tags'>
+						{this.props.tags.map((tag,i) => <PropertyTag key={this.props.id+i} {...tag} />)}
 					</div>
-				</div>
-				<div className='card-tags'>
-					{tags.map((tag,i) => <PropertyTag key={id+i} {...tag} />)}
-				</div>
-				<div className='card-footer'>
-					<div className='capacity'>
-						<span>Up to {attributes.max_capacity} guests</span>
-					</div>
-					<div className='divider' />
-					<div className='duration'>
-						<span>{ attributes.min_duration / 60} hours minimum</span>
+					<div className='card-footer'>
+						<div className='capacity'>
+							<span>Up to {this.props.attributes.max_capacity} guests</span>
+						</div>
+						<div className='divider' />
+						<div className='duration'>
+							<span>{this.props.attributes.min_duration / 60} hours minimum</span>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default PropertyListItem;
