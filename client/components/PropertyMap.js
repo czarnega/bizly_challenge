312149395d50ece42f5ee5231385528de313f5d6
Colin Zarnegar <c.zarnegar@gmail.com>
2016-08-05 22:47:18 -0700

    adds css for leaflet map and completes initial render of markers

--- client/components/PropertyMap.js
+++ client/components/PropertyMap.js
@@ -5,30 +5,38 @@ import { divIcon } from 'leaflet';
 
 
 // hard set position for example		
-const position = [
-	40.751329, 
-	-73.986701,
-];
+const position = {
+	'New York': [40.751329, -73.986701],
+	'San Francisco': [37.785200, -122.420902]
+};
 
 class PropertyMap extends Component {
 	render() {
 		return (
 			<div className='property-map-container' >			
-				<Map className='property-map' center={position} zoom={13} >
+				<Map className='property-map' center={position[this.props.defaultLocation]} zoom={13} >
 					<TileLayer
 						url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
 						attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 					/>
-
+					{this.props.properties.map(property => (
+						<Marker
+							key={property.id}
+							position={[property.attributes.lat, property.attributes.lng]}
+							title={property.attributes.name}
+							riseOnHover={true}
+						/>
+					))}
 				</Map>
 			</div>
 		);
 	}
 }
 
-function mapStateToProps(state) {
-	return state;
-}
+const mapStateToProps = (state) => ({
+	properties: state.properties,
+	defaultLocation: state.filters.location
+})
 
 export default connect(mapStateToProps, null)(PropertyMap);
 
